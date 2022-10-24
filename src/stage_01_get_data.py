c19761adc50aa54f8c4f704968a18d26982dcb4e
001DEEPANSHU <deepanshukaushik1998@gmail.com>
2022-10-25 00:09:12 +0530

    script race_data_to_mongo complete

---
+++ src/stage_01_get_data.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+from src.utils.mongo_operations import race_data_to_mongo
+import random
+import pymongo
+import json
+import requests
+
+STAGE = "Get Data"
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path, params_path, secrets_path):
+    ## reading config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    secrets = read_yaml(secrets_path)
+
+    conn_str = secrets["mongodb"]["url"].format(secrets["mongodb"]["username"],secrets["mongodb"]["pwd"])
+    print(conn_str)
+    connect = pymongo.MongoClient(conn_str)
+
+
+    race_data_to_mongo(conn_str,connect,config)
+
+
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--secrets", "-s", default="configs/secrets.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params, secrets_path=parsed_args.secrets)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
